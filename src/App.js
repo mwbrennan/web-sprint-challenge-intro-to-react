f5653f9d7ad5a4db38e409b5977b4bb4a5645e67
mwbrennan <mwb5@me.com>
2021-01-22 11:56:53 -0600

    mvp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
+import styled from 'styled-components';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [person, setPerson] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+    .get('https://swapi.dev/api/people/?')
+    .then(res => {
+      setPerson(res.data.results)
+      // console.log(res.data.results)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, []);
+
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters from Star Wars</h1>
+      {
+        person.map((data, i) => {
+          console.log(data.name)
+          return <Character key = {i} person = {data}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function Character (props) {
+
+    // console.log(props);
+
+    return (
+        <DivStyle>
+            <h2>{props.person.name}</h2>
+            <p>Mass: {props.person.mass}</p>
+        </DivStyle>
+    )
+
+} 
+
+const DivStyle = styled.div`
+    background-color: tan;
+    width: 50%;
+    margin: auto;
+    display: flex;
+    margin-bottom: 14px;
+    border-radius: 8px;
+
+    h2 {
+        padding: 12px;
+        justify-content: flex-start;
+        margin-left: 2%;
+        color: darkolivegreen;
+    }
+
+    p {
+        display: flex;
+        justify-content: flex-end;
+        flex-grow: 1;
+        margin-right: 5%;
+        padding-top: 20px;
+        font-weight: bold;
+        color: darkolivegreen;
+    }
+`;
